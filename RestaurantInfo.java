b47c126f17d0fe79bd1a89eab9313dfd2c1ab77a
LoOO1224 <landofoo2020@gmail.com>
2026-03-22 11:43:49 +0900

    Update RestaurantInfo.java with actual code

---
+++ RestaurantInfo.java
@@ -0,0 +1,7 @@
+public class RestaurantInfo {
+    public static void main(String[] args) {
+        System.out.println("Starbucks Gangnam Station");
+        System.out.println("390 Gangnam-daero, Gangnam-gu");
+        System.out.println("Seoul, South Korea 06132");
+    }
+}
